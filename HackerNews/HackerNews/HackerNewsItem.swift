cd3f2d13c89658648e6a2eb906bd87401ce0b5cf
Benjamin Lewis <23inhouse@gmail.com>
2019-06-29 16:04:03 +0200

    Add HackerNewsFilter

---
+++ HackerNews/HackerNews/HackerNewsItem.swift
@@ -0,0 +1,14 @@
+//
+//  HackerNewsItem.swift
+//  HackerNews
+//
+//  Created by Benjamin Lewis on 29/6/19.
+//  Copyright © 2019 Benjamin Lewis. All rights reserved.
+//
+
+import Foundation
+
+struct HackerNewsItem {
+    let title: String
+    let commentCount: Int
+}
