98ee279c038c615931e2e0a16424578269ea451d
Benjamin Lewis <23inhouse@gmail.com>
2019-07-09 11:16:44 +0200

    Update styled cell label for attributed content

--- HackerNews/HackerNews/NewsItems/StyledCellLabel.swift
+++ HackerNews/HackerNews/NewsItems/StyledCellLabel.swift
@@ -10,21 +10,34 @@ import UIKit
 
 class StyledCellLabel: UILabel {
     var textContent: String = " " {
-        didSet {
-            placeHolderColor = textContent == " " ? placeHolderColor : .white
-            backgroundColor = placeHolderColor
-            text = textContent
-            accessibilityLabel = textContent
-        }
+        didSet { setText(self, textContent) }
+    }
+
+    var attributedContent: NSAttributedString = NSAttributedString(string: " ") {
+        didSet { setAttributed(self, attributedContent) }
+    }
+
+    var setText: (StyledCellLabel, String) -> Void = { (self, textContent) in
+        self.placeHolderColor = textContent == " " ? self.placeHolderColor : .white
+        self.backgroundColor = self.placeHolderColor
+        self.text = textContent
+        self.accessibilityLabel = textContent
+    }
+
+    var setAttributed: (StyledCellLabel, NSAttributedString) -> Void = { (self, textContent) in
+        self.placeHolderColor = textContent == NSAttributedString(string: " ") ? self.placeHolderColor : .white
+        self.backgroundColor = self.placeHolderColor
+        self.attributedText = textContent
+        self.accessibilityLabel = textContent.string
     }
 
     var placeHolderColor: UIColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        backgroundColor = placeHolderColor
-        text = textContent
-        accessibilityLabel = textContent
+        attributedContent.length > 0
+            ? setAttributed(self, attributedContent)
+            : setText(self, textContent)
     }
 
     required init?(coder aDecoder: NSCoder) {
--- HackerNews/HackerNewsTests/NewsItems/StyledCellLabelTests.swift
+++ HackerNews/HackerNewsTests/NewsItems/StyledCellLabelTests.swift
@@ -13,15 +13,26 @@ class StyledCellLabelTests: XCTestCase {
     func testNullState() {
         let label = StyledCellLabel()
         XCTAssertEqual(label.text, " ", "The text should be ' '")
+        XCTAssertEqual(label.attributedText?.string, " ")
         XCTAssertEqual(label.accessibilityLabel, " ", "The accessibilityLabel should be ' '")
         XCTAssertEqual(label.backgroundColor, #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1), "Wrong background color")
     }
 
     func testSettingTextContent() {
         let label = StyledCellLabel()
-        label.textContent = "Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!"
-        XCTAssertEqual(label.text, "Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!", "The text should be ' '")
-        XCTAssertEqual(label.accessibilityLabel, "Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!", "The accessibiblity label should be ' '")
+        label.textContent = "Does your lorem ipsum ..."
+        XCTAssertEqual(label.text, "Does your lorem ipsum ...", "The text should be 'Does your lorem ipsum ...'")
+        XCTAssertEqual(label.attributedText?.string, "Does your lorem ipsum ...", "The attributed text should be 'Does your lorem ipsum ...'")
+        XCTAssertEqual(label.accessibilityLabel, "Does your lorem ipsum ...", "The accessibiblity label should be 'Does your lorem ipsum ...'")
+        XCTAssertEqual(label.backgroundColor, .white, "Wrong background color")
+    }
+
+    func testSettingAttributedContent() {
+        let label = StyledCellLabel()
+        label.attributedContent = NSAttributedString(string: "Does your lorem ipsum ...")
+        XCTAssertEqual(label.text, "Does your lorem ipsum ...", "The text should be 'Does your lorem ipsum ...'")
+        XCTAssertEqual(label.attributedText, NSAttributedString(string: "Does your lorem ipsum ..."), "The attributed text should be 'Does your lorem ipsum ...'")
+        XCTAssertEqual(label.accessibilityLabel, "Does your lorem ipsum ...", "The accessibiblity label should be 'Does your lorem ipsum ...'")
         XCTAssertEqual(label.backgroundColor, .white, "Wrong background color")
     }
 }
